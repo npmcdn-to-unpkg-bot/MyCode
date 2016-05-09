64b094db201fd39167e9a6c90557a514616933e8
xjsaber <xjsaber@yeah.net>
2016-05-09 09:42:13 +0800

    TLMManager add unituser、pinyinhelper

---
+++ DotNet/WebApi/TLMManager/TLMManager/TLMManager.Tests/UnitUser.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TLMManager.Controller;
+using TLMManager.Service;
+
+namespace TLMManager.Tests
+{
+    [TestClass]
+    public class UnitUser
+    {
+        [TestMethod]
+        public void GetList()
+        {
+            ModelInject.Init();//ioc
+            var unit = new UserController();
+
+            var list = unit.GetList();
+            Assert.AreNotEqual(list.Count(), 0);
+        }
+    }
+}
