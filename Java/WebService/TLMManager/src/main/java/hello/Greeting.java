63a5be6671a90ef359c950402d3a72506f94c59a
xjsaber <xjsaber@hotmail.com>
2016-05-11 23:04:39 +0800

    TLMIO add java TLMManager

---
+++ Java/WebService/TLMManager/src/main/java/hello/Greeting.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Greeting{
+
+    private final long id;
+    private final string content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
