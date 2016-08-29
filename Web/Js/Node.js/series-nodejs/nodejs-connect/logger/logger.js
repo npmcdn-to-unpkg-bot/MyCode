83c45f03ebf4a5518a68015e125d9ef522ce296c
xjsaber <xjsaber@hotmail.com>
2016-08-29 23:08:29 +0800

    sync

--- Web/Js/Node.js/series-nodejs/nodejs-connect/logger/logger.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var logger = require('morgan');
-var app = connect()
-    .use(logger("combined"))
-    .use(function (req, res) {
-        res.end('hello world\n');
-    })
-    .listen(3000);
